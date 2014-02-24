d8c1d804a11a4a9d42b470704b40dcb8d4168b7a
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2014-02-24 14:54:11 -0600

    Adds Jframe class and playground

--- TiroParabolico/src/tiroparabolico/TiroParabolico.java
+++ TiroParabolico/src/tiroparabolico/TiroParabolico.java
@@ -6,17 +6,21 @@
 
 package tiroparabolico;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author Ferrufino
  */
 public class TiroParabolico {
 
-    /**
+ /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        PlayGround variable = new PlayGround();//creo un objeto
+        variable.setVisible(true); //Aparezca mi codigo en clase AppletExamen1
+        variable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
